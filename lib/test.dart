3b13d6a93495acacf43dbb115578d73ef878d443
sujal880 <sujaldave880@gmail.com>
2024-06-15 09:16:00 +0530

    Row Column

--- lib/test.dart
+++ lib/test.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(Test());
 }
-class Test extends StatelessWidget{
+
+class Test extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,28 +14,52 @@ class Test extends StatelessWidget{
   }
 }
 
-class FirstScreen extends StatelessWidget{
-  int count=0;
+class FirstScreen extends StatelessWidget {
+  int count = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text("First Screen"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Container(
-          height: 100,
-          width: 100,
-          decoration: BoxDecoration(
-              color: Colors.red,
-            borderRadius: BorderRadius.circular(12)
-          ),
-          child: Center(child: Text("Hello World!!")),
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text("First Screen"),
+          centerTitle: true,
         ),
-      )
+        body: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Column(children: [
+            Container(
+              height: 100,
+              width: 200,
+              color: Colors.red,
+            ),
+            Container(
+              height: 100,
+              width: 200,
+              color: Colors.blue,
+            ),
+            Container(
+              height: 100,
+              width: 200,
+              color: Colors.red,
+            ),
+
+
+          ],),
+        ),
+        // body: Center(
+        //   child: Container(
+        //     clipBehavior: Clip.antiAlias,
+        //     height: 200,
+        //     width: 200,
+        //     decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
+        //     // child: Image.asset("assets/images/sujal.jpg",fit: BoxFit.cover,),
+        //     child: Image.network(
+        //       "https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-169994-674010.jpg&fm=jpg",
+        //       fit: BoxFit.cover,
+        //     ),
+        //   ),
+        // ));
     );
   }
-
+  //https://media.geeksforgeeks.org/wp-content/uploads/20201026003622/ezgifcomgifmaker3.gif
 }
